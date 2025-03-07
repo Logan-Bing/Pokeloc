37a691f7bbafef1ecef7d6461d9dba3f0a7ee41c
Michael Souvannarath <msouvann@gmail.com>
2025-03-07 11:16:40 +0100

    toggleboard added

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
