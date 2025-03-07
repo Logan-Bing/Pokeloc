a968c411c93d553f43db6e294be1ce75f76e6d77
logan lugez <binglei.pro@gmail.com>
2025-03-07 10:50:17 +0100

    config toggle

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,12 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["button"]
+  static targets = ["arrow", "button"]
   connect() {
     console.log(this.buttonTarget)
   }
 
   fire(){
     this.buttonTarget.classList.toggle('d-none')
+
+    this.arrowTarget.classList.toggle("rotate")
   }
+
+
 }
